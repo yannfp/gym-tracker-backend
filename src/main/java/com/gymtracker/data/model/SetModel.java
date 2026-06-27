1e6c0f35786d9ca6dc9eab5c7b9c8d3edbfdb5a7
Yann Fernandez Puig <yann.fernandez-puig@epita.fr>
2026-06-27 22:19:54 +0200

    feat(SetModel.java)

---
+++ src/main/java/com/gymtracker/data/model/SetModel.java
@@ -0,0 +1,27 @@
+package com.gymtracker.data.model;
+
+import java.util.UUID;
+
+import jakarta.persistence.*;
+
+@Entity
+public class SetModel {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  @Column(name = "id", nullable = false, updatable = false)
+  public UUID id;
+
+  @Column(name = "set_number", nullable = false)
+  public Integer setNumber;
+
+  @Column(name = "repetitions", nullable = false)
+  public Integer repetitions;
+
+  @Column(name = "weight", nullable = false)
+  public Integer weight;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "workout_exercise_id", nullable = false)
+  public WorkoutExerciseModel exercise;
+}
