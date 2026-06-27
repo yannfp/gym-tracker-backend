dd8ad17fc403aca22155d00ad569169f9472a3b5
Yann Fernandez Puig <yann.fernandez-puig@epita.fr>
2026-06-27 22:19:24 +0200

    feat(ExerciseModel.java)

---
+++ src/main/java/com/gymtracker/data/model/ExerciseModel.java
@@ -0,0 +1,23 @@
+package com.gymtracker.data.model;
+
+import java.util.*;
+import jakarta.persistence.*;
+
+@Entity
+public class ExerciseModel {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  @Column(name = "id", nullable = false, updatable = false)
+  public UUID id;
+
+  @Column(name = "name", nullable = false)
+  public String name;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "main_muscle_group", nullable = false)
+  public MuscleGroup mainMuscleGroup;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "secondary_muscle_group")
+  public MuscleGroup secondaryMuscleGroup;
+}
