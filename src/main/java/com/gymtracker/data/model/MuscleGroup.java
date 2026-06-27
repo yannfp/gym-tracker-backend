03f036062e5da74e5a7cbf397e4dfe4c4487d6bd
Yann Fernandez Puig <yann.fernandez-puig@epita.fr>
2026-06-27 22:19:41 +0200

    feat(MuscleGroup.java)

---
+++ src/main/java/com/gymtracker/data/model/MuscleGroup.java
@@ -0,0 +1,15 @@
+package com.gymtracker.data.model;
+
+public enum MuscleGroup {
+  CHEST,
+  BACK,
+  SHOULDERS,
+  BICEPS,
+  TRICEPS,
+  FOREARMS,
+  CORE,
+  QUADS,
+  HAMSTRINGS,
+  GLUTES,
+  CALVES
+}
