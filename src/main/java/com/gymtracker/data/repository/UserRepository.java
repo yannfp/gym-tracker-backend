a7f01fc953433f8942b27c4d4a0d54d9831cb7e0
Yann Fernandez Puig <yann.fernandez-puig@epita.fr>
2026-06-28 11:41:38 +0200

    feat(UserRepository.java): find user by uuid

--- src/main/java/com/gymtracker/data/repository/UserRepository.java
+++ src/main/java/com/gymtracker/data/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.gymtracker.data.repository;
 
+import java.util.UUID;
+
 import com.gymtracker.data.model.UserModel;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
@@ -8,4 +10,7 @@ import jakarta.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class UserRepository implements PanacheRepository<UserModel> {
 
+  UserModel findById(UUID id) {
+    return find("id", id).firstResult();
+  }
 }
