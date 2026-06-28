722d570224095706e7633c93c994ab37413ab70a
Yann Fernandez Puig <yann.fernandez-puig@epita.fr>
2026-06-28 11:58:15 +0200

    feat(UserService.java): find user by id, email and username

---
+++ src/main/java/com/gymtracker/domain/service/UserService.java
@@ -0,0 +1,27 @@
+package com.gymtracker.domain.service;
+
+import java.util.UUID;
+
+import com.gymtracker.data.model.UserModel;
+import com.gymtracker.data.repository.UserRepository;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+@ApplicationScoped
+public class UserService {
+  @Inject
+  UserRepository userRepository;
+
+  public UserModel findById(UUID id) {
+    return userRepository.findById(id);
+  }
+
+  public UserModel findByEmail(String email) {
+    return userRepository.findByEmail(email);
+  }
+
+  public UserModel findByUsername(String username) {
+    return userRepository.findByUsername(username);
+  }
+}
