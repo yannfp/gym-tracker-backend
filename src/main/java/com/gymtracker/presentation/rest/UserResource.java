974fd4f59561dedcda5c3afba6490ef8e873bea2
Yann Fernandez Puig <yann.fernandez-puig@epita.fr>
2026-06-28 19:26:05 +0200

    feat(UserResource.java): endpoint to update the user

--- src/main/java/com/gymtracker/presentation/rest/UserResource.java
+++ src/main/java/com/gymtracker/presentation/rest/UserResource.java
@@ -10,13 +10,16 @@ import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import com.gymtracker.converter.UserConverter;
 import com.gymtracker.data.model.UserModel;
 import com.gymtracker.domain.service.UserService;
+import com.gymtracker.presentation.api.request.UpdateUserRequest;
 import com.gymtracker.presentation.api.response.UserResponse;
 
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
+import jakarta.validation.Valid;
 import jakarta.ws.rs.BadRequestException;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
@@ -44,12 +47,21 @@ public class UserResource {
   public Response me() {
     UUID userId = UUID.fromString(jwt.getSubject());
 
-    UserModel user = userService.findById(userId);
-    if (user == null) {
-      throw new BadRequestException("User not found");
-    }
+    UserResponse response = userService.findbyId(userId);
 
-    UserResponse response = userConverter.toUserResponse(user);
+    return Response.ok(response).build();
+  }
+
+  @PUT
+  @Path("/me")
+  @RolesAllowed("/user")
+  @APIResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = UserResponse.class)))
+  @APIResponse(responseCode = "400", description = "User not found")
+  public Response updateMe(@Valid UpdateUserRequest request) {
+    UUID userId = UUID.fromString(jwt.getSubject());
+
+    UserResponse response = userService.updateUser(userId, request.email, request.username, request.password,
+        request.firstname, request.lastname);
 
     return Response.ok(response).build();
   }
