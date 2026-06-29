e5d01ce62eda013bd493eacead253aa9f4883079
Yann Fernandez Puig <yann.fernandez-puig@epita.fr>
2026-06-29 18:19:08 +0200

    feat(ExerciseRepository.java): fetch all exercises

--- src/main/java/com/gymtracker/data/repository/ExerciseRepository.java
+++ src/main/java/com/gymtracker/data/repository/ExerciseRepository.java
@@ -1,5 +1,7 @@
 package com.gymtracker.data.repository;
 
+import java.util.List;
+
 import com.gymtracker.data.model.ExerciseModel;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
@@ -8,4 +10,7 @@ import jakarta.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class ExerciseRepository implements PanacheRepository<ExerciseModel> {
 
+  public List<ExerciseModel> fetchExercises() {
+    return listAll();
+  }
 }
