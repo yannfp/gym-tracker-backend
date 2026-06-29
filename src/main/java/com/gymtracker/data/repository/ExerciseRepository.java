15b9fd6d6a88b1e932df836840405aa186fb8588
Yann Fernandez Puig <yann.fernandez-puig@epita.fr>
2026-06-29 19:07:37 +0200

    feat(ExerciseRepository.java): filter exercises by muscle group

--- src/main/java/com/gymtracker/data/repository/ExerciseRepository.java
+++ src/main/java/com/gymtracker/data/repository/ExerciseRepository.java
@@ -3,9 +3,11 @@ package com.gymtracker.data.repository;
 import java.util.List;
 
 import com.gymtracker.data.model.ExerciseModel;
+import com.gymtracker.data.model.MuscleGroup;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.NotFoundException;
 
 @ApplicationScoped
 public class ExerciseRepository implements PanacheRepository<ExerciseModel> {
@@ -16,5 +18,14 @@ public class ExerciseRepository implements PanacheRepository<ExerciseModel> {
 
   public ExerciseModel findByName(String name) {
     return find("name", name).firstResult();
+  }
+
+  public List<ExerciseModel> filter(String category) {
+    try {
+      MuscleGroup group = MuscleGroup.valueOf(category.toUpperCase());
+      return list("mainMuscleGroup", group);
+    } catch (IllegalArgumentException | NullPointerException e) {
+      throw new NotFoundException("Muscle category not found");
+    }
   }
 }
