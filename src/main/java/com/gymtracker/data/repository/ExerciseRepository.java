3883f6e3f6b4da542a7fdf5f57ff1df7100dd58d
Yann Fernandez Puig <yann.fernandez-puig@epita.fr>
2026-06-29 21:39:55 +0200

    feat(ExerciseRepository.java): delete exercise by id

--- src/main/java/com/gymtracker/data/repository/ExerciseRepository.java
+++ src/main/java/com/gymtracker/data/repository/ExerciseRepository.java
@@ -1,17 +1,18 @@
 package com.gymtracker.data.repository;
 
 import java.util.List;
+import java.util.UUID;
 
 import com.gymtracker.data.model.ExerciseModel;
 import com.gymtracker.data.model.MuscleGroup;
 
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.NotFoundException;
 
 @ApplicationScoped
-public class ExerciseRepository implements PanacheRepository<ExerciseModel> {
+public class ExerciseRepository implements PanacheRepositoryBase<ExerciseModel, UUID> {
 
   public List<ExerciseModel> fetchExercises() {
     return listAll();
@@ -33,5 +34,10 @@ public class ExerciseRepository implements PanacheRepository<ExerciseModel> {
   @Transactional
   public void createExercise(ExerciseModel exercise) {
     persist(exercise);
+  }
+
+  @Transactional
+  public Boolean deleteExercise(UUID id) {
+    return deleteById(id);
   }
 }
