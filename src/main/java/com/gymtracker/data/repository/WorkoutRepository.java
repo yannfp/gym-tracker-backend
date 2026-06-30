b6e3ad0afc14238992327e25c4f416045e408f5e
Yann Fernandez Puig <yann.fernandez-puig@epita.fr>
2026-06-30 11:48:12 +0200

    feat(WorkoutRepository.java): fetch all workouts from an user

--- src/main/java/com/gymtracker/data/repository/WorkoutRepository.java
+++ src/main/java/com/gymtracker/data/repository/WorkoutRepository.java
@@ -1,11 +1,17 @@
 package com.gymtracker.data.repository;
 
+import java.util.List;
+import java.util.UUID;
+
 import com.gymtracker.data.model.WorkoutModel;
 
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-public class WorkoutRepository implements PanacheRepository<WorkoutModel> {
+public class WorkoutRepository implements PanacheRepositoryBase<WorkoutModel, UUID> {
 
+  public List<WorkoutModel> fetchUserWorkouts(UUID id) {
+    return list("user.id", id);
+  }
 }
