6b042781674563afbd37ddfdeffe1eb95c15ad8c
Yann Fernandez Puig <yann.fernandez-puig@epita.fr>
2026-06-30 11:34:59 +0200

    feat(WorkoutExerciseConverter.java)

---
+++ src/main/java/com/gymtracker/converter/WorkoutExerciseConverter.java
@@ -0,0 +1,38 @@
+package com.gymtracker.converter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.gymtracker.data.model.WorkoutExerciseModel;
+import com.gymtracker.presentation.api.response.SetResponse;
+import com.gymtracker.presentation.api.response.WorkoutExerciseResponse;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+@ApplicationScoped
+public class WorkoutExerciseConverter {
+
+  @Inject
+  SetConverter setConverter;
+
+  public WorkoutExerciseResponse toResponse(WorkoutExerciseModel workoutExercise) {
+    List<SetResponse> sets = setConverter.toResponseList(workoutExercise.sets);
+
+    return new WorkoutExerciseResponse(
+        workoutExercise.id,
+        workoutExercise.exercise.id,
+        workoutExercise.exercise.name,
+        workoutExercise.exercise.mainMuscleGroup.toString(),
+        workoutExercise.exercise.secondaryMuscleGroup.toString(),
+        sets);
+  }
+
+  public List<WorkoutExerciseResponse> toResponseList(List<WorkoutExerciseModel> workoutExercises) {
+    List<WorkoutExerciseResponse> response = new ArrayList<>();
+
+    workoutExercises.forEach(exercise -> response.add(toResponse(exercise)));
+
+    return response;
+  }
+}
