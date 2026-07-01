279dc9dcd6efeda8ad1568827580a59821ec393c
Yann Fernandez Puig <yann.fernandez-puig@epita.fr>
2026-07-01 22:59:20 +0200

    fix: imports

--- src/main/java/com/gymtracker/presentation/api/request/AddSetRequest.java
+++ src/main/java/com/gymtracker/presentation/api/request/AddSetRequest.java
@@ -1,7 +1,5 @@
 package com.gymtracker.presentation.api.request;
 
-import jakarta.validation.constraints.NotBlank;
-
 public class AddSetRequest {
   public Integer repetitions;
   public Double weight;
